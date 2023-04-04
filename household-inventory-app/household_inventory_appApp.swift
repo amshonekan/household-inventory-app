f6008c54f280dbf151d05d233b03f7486a1288a0
amshonekan <a.shonekan96@gmail.com>
2023-04-04 03:43:25 +0100

    Initial Commit

---
+++ household-inventory-app/household_inventory_appApp.swift
@@ -0,0 +1,20 @@
+//
+//  household_inventory_appApp.swift
+//  household-inventory-app
+//
+//  Created by The Dragon on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct household_inventory_appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
